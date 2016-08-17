527980cd6bf60d9474fe1957bddf7a593802240b
Julien Vollweiter <viergelenker@web.de>
2016-08-18 00:34:03 +0200

    Added the slick plugin for carousels, mobile menu modified

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -4,15 +4,26 @@
 $(document).foundation();
 
 // ===== Scroll to Top ====
-$(window).scroll(function() {
+$(window).scroll(function () {
     if ($(this).scrollTop() >= 100) {        // If page is scrolled more than 50px
         $('#return-to-top').fadeIn(200);    // Fade in the arrow
     } else {
         $('#return-to-top').fadeOut(200);   // Else fade out the arrow
     }
 });
-$('#return-to-top').click(function() {      // When arrow is clicked
+$('#return-to-top').click(function () {      // When arrow is clicked
     $('body,html').animate({
-        scrollTop : 0                       // Scroll to top of body
+        scrollTop: 0                       // Scroll to top of body
     }, 500);
+});
+
+
+$(document).ready(function(){
+    $('.carousel').slick({
+        autoplay: true,
+        infinite: true,
+        autoplaySpeed: 5000,
+        dots: true,
+        pauseOnDotsHover: true,
+});
 });
