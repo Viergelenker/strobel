466aefff783c69d268e7247ed218c592feed2ae1
Julien Vollweiter <viergelenker@web.de>
2016-11-26 23:52:54 +0100

    Removed arrows of carousel

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,1 +1,7 @@
-$(document).foundation(),$(window).scroll(function(){$(this).scrollTop()>=100?$("#return-to-top").fadeIn(200):$("#return-to-top").fadeOut(200)}),$("#return-to-top").click(function(){$("body,html").animate({scrollTop:0},500)}),$(document).ready(function(){$(".carousel").slick({autoplay:!0,infinite:!0,autoplaySpeed:5e3})});
+$(document).foundation(), $(window).scroll(function () {
+    $(this).scrollTop() >= 100 ? $("#return-to-top").fadeIn(200) : $("#return-to-top").fadeOut(200)
+}), $("#return-to-top").click(function () {
+    $("body,html").animate({scrollTop: 0}, 500)
+}), $(document).ready(function () {
+    $(".carousel").slick({autoplay: !0, infinite: !0, autoplaySpeed: 5e3, arrows: false})
+});
